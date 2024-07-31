2d4858a408142e1cbf3d9a2721c284078619a084
Mark Lipscombe <mark@lipscombe.com>
2024-07-31 17:59:25 -0400

    adjust existing iscsi target error detection for newer TrueNAS SCALE versions

--- src/driver/freenas/api.js
+++ src/driver/freenas/api.js
@@ -836,12 +836,14 @@ class FreeNASApiDriver extends CsiBaseDriver {
                 target
               );
 
-              // 409 or 422 if invalid
+              // 409 if invalid
               if (response.statusCode != 201) {
                 target = null;
                 if (
-                  (response.statusCode == 409 || response.statusCode == 422) &&
-                  JSON.stringify(response.body).includes("already exists")
+                  response.statusCode == 409 &&
+                  JSON.stringify(response.body).includes(
+                    "Target name already exists"
+                  )
                 ) {
                   target = await httpApiClient.findResourceByProperties(
                     "/services/iscsi/target",
@@ -1127,9 +1129,7 @@ class FreeNASApiDriver extends CsiBaseDriver {
                 target = null;
                 if (
                   response.statusCode == 422 &&
-                  JSON.stringify(response.body).includes(
-                    "Target name already exists"
-                  )
+                  JSON.stringify(response.body).includes("already exists")
                 ) {
                   target = await httpApiClient.findResourceByProperties(
                     "/iscsi/target",
